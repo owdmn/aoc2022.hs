2e067300a3cd4bb0f6110c05615f18402a3b294b
Oliver Wiedemann <mail@oliver-wiedemann.net>
2022-12-03 00:32:58 +0100

    add solution for 2b

--- src/2/Main.hs
+++ src/2/Main.hs
@@ -1,39 +1,58 @@
+{-# LANGUAGE DeriveAnyClass #-}
+
+class (Eq a, Enum a, Bounded a) => CyclicEnum a where
+    cpred :: a -> a
+    cpred x
+        | x == minBound = maxBound
+        | otherwise = pred x
+    csucc :: a -> a
+    csucc x
+        | x == maxBound = minBound
+        | otherwise = succ x
+
 data RPS = R | P | S
-    deriving (Eq, Enum, Show)
-   
+    deriving (Eq, Enum, Bounded, CyclicEnum, Show)
+
 data Result = Lose | Tie | Win
     deriving (Eq, Enum, Show)
 
 -- decide who wins, opponent goes first
 defeats :: RPS -> RPS -> Result
-defeats P R = Lose
-defeats R P = Win
-defeats S P = Lose
-defeats P S = Win
-defeats R S = Lose
-defeats S R = Win
-defeats _ _ = Tie
+defeats x y
+    | csucc x == y = Win
+    | cpred x == y = Lose
+    | otherwise = Tie
 
-shape :: (RPS, RPS) -> Int
-shape x = 1 + fromEnum (snd x)
+score :: (RPS, RPS) -> Int
+score x = 1 + fromEnum (snd x) + 3 * fromEnum (uncurry defeats x)
 
-result :: (RPS, RPS) -> Int
-result x = 3 * (fromEnum $ defeats (fst x) (snd x))
+p :: Char -> RPS
+p c
+    | c `elem` "AX" = R
+    | c `elem` "BY" = P
+    | c `elem` "CZ" = S
+    | otherwise = undefined
 
 parse :: String -> (RPS, RPS)
-parse x = (p (x!!0), p (x!!2))
+parse x = (p (head x), p (x!!2))
+
+parse' :: String -> (RPS, RPS)
+parse' x = (p (head x), pickMove (p (head x)) (q (x!!2)))
     where 
-        p 'A' = R
-        p 'X' = R
-        p 'B' = P
-        p 'Y' = P
-        p 'C' = S
-        p 'Z' = S
-        p  _  = undefined
+        q :: Char -> Result
+        q 'X' = Lose
+        q 'Y' = Tie
+        q 'Z' = Win
+        q  _  = undefined
+
+pickMove :: RPS -> Result -> RPS
+pickMove x Win = csucc x
+pickMove x Lose = cpred x
+pickMove x Tie = x
 
 main :: IO ()
 main = do
     input <- readFile "src/2/input"
-    let d = parse <$> lines input
-    putStrLn $ show $ sum $ (shape <$> d) ++ (result <$> d)
+    print $ sum $ score . parse  <$> lines input
+    print $ sum $ score . parse' <$> lines input
     return ()
